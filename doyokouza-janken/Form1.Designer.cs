ee2b207ecdb928323337a87b54e3c61ac625a41b
Admin <s8kosaka02@outlook.jp>
2021-08-08 23:43:09 +0900

    プロジェクト ファイルを追加します。

---
+++ doyokouza-janken/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace doyokouza_janken
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rock = new System.Windows.Forms.Button();
+            this.scissors = new System.Windows.Forms.Button();
+            this.paper = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rock
+            // 
+            this.rock.Location = new System.Drawing.Point(12, 212);
+            this.rock.Name = "rock";
+            this.rock.Size = new System.Drawing.Size(114, 137);
+            this.rock.TabIndex = 0;
+            this.rock.Text = "グー";
+            this.rock.UseVisualStyleBackColor = true;
+            this.rock.Click += new System.EventHandler(this.rock_Click);
+            // 
+            // scissors
+            // 
+            this.scissors.Location = new System.Drawing.Point(132, 212);
+            this.scissors.Name = "scissors";
+            this.scissors.Size = new System.Drawing.Size(120, 137);
+            this.scissors.TabIndex = 0;
+            this.scissors.Text = "チョキ";
+            this.scissors.UseVisualStyleBackColor = true;
+            this.scissors.Click += new System.EventHandler(this.scissors_Click);
+            // 
+            // paper
+            // 
+            this.paper.Location = new System.Drawing.Point(258, 212);
+            this.paper.Name = "paper";
+            this.paper.Size = new System.Drawing.Size(114, 137);
+            this.paper.TabIndex = 0;
+            this.paper.Text = "パー";
+            this.paper.UseVisualStyleBackColor = true;
+            this.paper.Click += new System.EventHandler(this.paper_Click);
+            // 
+            // picture
+            // 
+            this.picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture.Location = new System.Drawing.Point(12, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(360, 194);
+            this.picture.TabIndex = 1;
+            this.picture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.paper);
+            this.Controls.Add(this.scissors);
+            this.Controls.Add(this.rock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rock;
+        private System.Windows.Forms.Button scissors;
+        private System.Windows.Forms.Button paper;
+        private System.Windows.Forms.PictureBox picture;
+    }
+}
+
---
+++ doyokouza-janken/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doyokouza_janken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public string 敵の手()
+        {
+            Random p = new Random();
+            int 乱数 = p.Next(1, 4); //1から3までの乱数を生成
+
+            string 出力;
+
+            if (乱数 == 1)
+            {
+                出力 = "グー";
+            }
+            else if (乱数 == 2)
+            {
+                出力 = "チョキ";
+            }
+            else
+            {
+                出力 = "パー";
+            }
+
+            return 出力;
+        }
+
+        public void 相手の画像(string 相手の手)
+        {
+            if (相手の手 == "グー")
+            {
+                picture.Image = Resource.gu;
+            }
+            else if (相手の手 == "チョキ")
+            {
+                picture.Image = Resource.choki;
+            }
+            else
+            {
+                picture.Image = Resource.pa;
+            }
+        }
+
+        public void 処理 (string 自分の手)
+        {
+
+            string 相手の手 = 敵の手();
+
+            Refresh(); //画像変更の前に画像を初期化
+            相手の画像(相手の手);
+            
+            string 結果 = "";
+
+            if (自分の手 == 相手の手)
+            {
+                結果 = "あいこ";
+            }
+            else if (自分の手 == "グー" && 相手の手 == "パー")
+            {
+                結果 = "負け";
+            }
+            else if (自分の手 == "グー" && 相手の手 == "チョキ ")
+            {
+                結果 = "勝ち";
+            }
+            else if (自分の手 == "チョキ" && 相手の手 == "グー")
+            {
+                結果 = "負け";
+            }
+            else if (自分の手 == "チョキ" && 相手の手 == "パー")
+            {
+                結果 = "勝ち";
+            }
+            else if (自分の手 == "パー" && 相手の手 == "グー")
+            {
+                結果 = "勝ち";
+            }
+            else if (自分の手 == "パー" && 相手の手 == "チョキ")
+            {
+                結果 = "負け";
+            }
+
+            MessageBox.Show("結果は " + 結果 + " です。");
+        }
+
+        public string 自分の手;
+
+        private void rock_Click(object sender, EventArgs e)
+        {
+            処理("グー");
+        }
+
+        private void scissors_Click(object sender, EventArgs e)
+        {
+            処理("チョキ");
+        }
+
+        private void paper_Click(object sender, EventArgs e)
+        {
+            処理("パー");
+        }
+    }
+}
